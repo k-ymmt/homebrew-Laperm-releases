2c8da8bbdd9445ef984e80382855fdcb11f658f1
k-ymmt <account@kymmt.net>
2026-04-14 19:36:05 +0900

    Update version to 0.1.0 and fix URLs for renamed repo

--- Casks/abyssinian.rb
+++ Casks/abyssinian.rb
@@ -1,11 +1,11 @@
 cask "abyssinian" do
-  version "1.0"
+  version "0.1.0"
   sha256 :no_check
 
-  url "https://github.com/k-ymmt/Abyssinian-releases/releases/latest/download/Abyssinian-#{version}.zip"
+  url "https://github.com/k-ymmt/homebrew-Abyssinian-releases/releases/latest/download/Abyssinian-#{version}.zip"
   name "Abyssinian"
   desc "Cross-platform markdown note-taking app"
-  homepage "https://k-ymmt.github.io/Abyssinian-releases/"
+  homepage "https://k-ymmt.github.io/homebrew-Abyssinian-releases/"
 
   auto_updates true
   depends_on macos: ">= :ventura"
